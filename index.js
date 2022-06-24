12b3fe08d327f7d5a06f691ad8cd3362dce950ee
danstacy1 <danstacy1@gmail.com>
2022-06-24 10:52:13 -0400

    cards are flipping but the image is not there

--- index.js
+++ index.js
@@ -28,6 +28,46 @@ const pictures = [
     },
     {
         name: 'hat',
+        imgFace: 'Image/hatpic.png', 
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'hotdog',
+        imgFace: 'Image/hotdogpic.png',
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'pizza',
+        imgFace: 'Image/pizzapic.jpeg',
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'umbrella',
+        imgFace: 'Image/umbrellapic.png',
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'wood',
+        imgFace: 'Image/woodpic.jpeg',
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'ball',
+        imgFace: 'Image/ballpic.png',
+        imgBack: 'Image/cardpic.jpeg',
+    }, 
+    {
+        name: 'chair',
+        imgFace: 'Image/chairpic.jpeg',
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'flag',
+        imgFace: 'Image/flagpic.jpeg',
+        imgBack: 'Image/cardpic.jpeg',
+    },
+    {
+        name: 'hat',
         imgFace: 'Image/hatpic.png',
         imgBack: 'Image/cardpic.jpeg',
     },
@@ -50,37 +90,46 @@ const pictures = [
         name: 'wood',
         imgFace: 'Image/woodpic.jpeg',
         imgBack: 'Image/cardpic.jpeg',
-    }]
+    }
+]
     
     // call all the divs from HTML
     const gameboard = document.querySelector('#gameboard')
     
     // Make 16 divs to represent the cards.
-    const makeGameCards = () => {
+    const makeCards = () => {
         // create 16 cards
         for (let i = 0; i < 16; i++) {
-            const face = document.createElement('img')
-            face.classList = 'face'
+            const card = document.createElement('div')
+            card.classList = 'card'
             const back = document.createElement('img')
             back.classList = 'back'
+            const face = document.createElement('img')
+            face.classList = 'face'
+            // face.setAttribute('data-name', pictures[i].name)
+            // face.setAttribute('src', pictures[i].imgFace)
+            // face.style.display = 'none'
+            // face.setAttribute('src', pictures[i].imgFace)
             if (i > 7) {
                 // array index should be i - 8.
-                face.setAttribute('data-name', pictures[i-8].name)
-                face.setAttribute('src', pictures[i-8].imgFace)
+                back.setAttribute('data-name', pictures[i-8].name)
+                back.setAttribute('src', pictures[i-8].imgBack)
             } else {
-                face.setAttribute('data-name', pictures[i].name)
-                face.setAttribute('src', pictures[i].imgFace)
+                back.setAttribute('data-name', pictures[i].name)
+                back.setAttribute('src', pictures[i].imgBack)
             }
-            face.setAttribute('data-id', i)
-            face.addEventListener('click', flipCard)
-            console.log('the card created', face)
-            gameboard.appendChild(face)
+            back.setAttribute('data-id', i)
+            back.addEventListener('click', flipCard)
+            console.log('the card created', back)
+            gameboard.appendChild(card)
+            card.appendChild(face)
+            card.appendChild(back)
         }
     }
     // add an event listener that will listen for 
     document.addEventListener("DOMContentLoaded", () => {
-        // now we need to call the makeGameCards function
-        makeGameCards()
+        // now we need to call the makeCards function
+        makeCards()
     })
     
 // Create an addEventListener to flip each card. 
@@ -92,6 +141,9 @@ const pictures = [
 // 2. if there is no first card, assign event target to first card.
 // 3. if one card is already selected, assign event target to the second card. 
 // const card = document.querySelectorAll('.img')
+document.querySelectorAll('face')
+face.setAttribute('data-name', pictures[i].name)
+face.setAttribute('src', pictures[i].imgFace)
 
 function flipCard (e) {
     console.log('this is the event', e)
@@ -99,13 +151,20 @@ function flipCard (e) {
     console.log('this is the card', theCard)
     const cardName = theCard.dataset.name
     console.log('this is cardname', cardName)
-    this.classList.toggle('flip')
+    // const imgFace = pictures.imgFace
+    // const face = getElementByClass('face')
     if (gameWinner == false && !firstCardPicked) {
         firstCardPicked = theCard
         console.log('one card selected')
+        // this.classList.toggle('face')
+        // face.setAttribute('data-name', pictures[i].name)
+        // face.setAttribute('src', pictures[i].imgFace)
+        this.setAttribute('src', pictures[theCard], 'face')
+        console.log(e.target.imgFace)
     }   else if (firstCardPicked !== null && !secondCardPicked) {
         secondCardPicked = theCard
         console.log('second card selected', secondCardPicked)
+        this.setAttribute('src', pictures[theCard], 'face')
         if (secondCardPicked !== null) {
             console.log ('two cards flipped')
             // checkforWin()
@@ -113,9 +172,7 @@ function flipCard (e) {
         }
     }
 }
-// theCard.forEach(theCard => theCard.addEventListener('click', flipCard,{
-//     once: true
-// }))
+// card.forEach(theCard => theCard.addEventListener('click', flipCard))
 
 
 // Create a function that checks of a matching pair
@@ -128,12 +185,12 @@ function flipCard (e) {
 // 4. if not a match, send a message that says try again, and flip the card back over, and then change fc and sc back to null.
  
 function checkForMatch() {
-    console.log('fc', firstCardPicked.dataset)
     const fcName = firstCardPicked.dataset.name
-    console.log('sc', secondCardPicked)
+    console.log('fc', firstCardPicked.dataset)
     const scName = secondCardPicked.dataset.name
+    console.log('sc', secondCardPicked)
     if (fcName === scName) {
-        face.target.removeEventListener('click', flipCard)
+        // this.target.removeEventListener('click', flipCard)
         console.log('its a match')
     } else {
         firstCardPicked = null
